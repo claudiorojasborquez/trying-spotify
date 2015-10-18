d955d2fcec13b7ab4d0653a0c46df3406db35fea
claudio rojas borquez <212degreesforboilingpoint@gmail.com>
2015-10-18 06:51:33 -0300

    Setting app/ First commit

---
+++ triying spotify/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace triying_spotify.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ triying spotify/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(triying_spotify.Startup))]
+namespace triying_spotify
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
